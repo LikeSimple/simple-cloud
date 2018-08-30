1da0adb2000245867c56b5b828db09ed91ae5f84
Jimmy Xu <wolfxz@foxmail.com>
2018-08-30 23:06:58 +0800

    add axios,element-ui,vuex and  scss, lower version browsers support

--- source/simple-cloud-admin/build/webpack.base.conf.js
+++ source/simple-cloud-admin/build/webpack.base.conf.js
@@ -22,7 +22,7 @@ const createLintingRule = () => ({
 module.exports = {
   context: path.resolve(__dirname, '../'),
   entry: {
-    app: './src/main.js'
+    app: ['babel-polyfill', './src/main.js']
   },
   output: {
     path: config.build.assetsRoot,
@@ -32,10 +32,10 @@ module.exports = {
       : config.dev.assetsPublicPath
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.js', '.vue', '.json', '.css', '.scss'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
-      '@': resolve('src'),
+      '@': resolve('src')
     }
   },
   module: {
@@ -74,6 +74,10 @@ module.exports = {
           limit: 10000,
           name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
         }
+      },
+      { // add sass support
+        test: /\.(css|scss)$/,
+        loader: 'style-loader!css-loader!sass-loader'
       }
     ]
   },
--- source/simple-cloud-admin/src/main.js
+++ source/simple-cloud-admin/src/main.js
@@ -3,13 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
+import Element from 'element-ui'
+import './assets/css/element-variables.scss'
 
 Vue.config.productionTip = false
+Vue.use(Element, {size: 'small'})
+
+// 使用路由器钩子函数对路由进行权限跳转
+router.beforeEach((to, from, next) => {
+  next()
+})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
