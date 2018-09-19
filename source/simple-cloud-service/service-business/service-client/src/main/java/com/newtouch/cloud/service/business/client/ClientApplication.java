3fe5afebe382a5392299b9ad306e53a04af65ba3
Jimmy Xu <zhen.xu@newtouch.cn>
2018-09-19 18:02:22 +0800

    完成eureka、config、zuul、zipkin基本配置

--- source/simple-cloud-service/service-business/service-client/src/main/java/com/newtouch/cloud/service/business/client/ClientApplication.java
+++ source/simple-cloud-service/service-business/service-client/src/main/java/com/newtouch/cloud/service/business/client/ClientApplication.java
@@ -2,11 +2,19 @@ package com.newtouch.cloud.service.business.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
+    }
+
+    @GetMapping(path = "/")
+    public String getName() {
+        return "Just for test.";
     }
 }
